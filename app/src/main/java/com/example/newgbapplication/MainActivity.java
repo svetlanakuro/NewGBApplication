e042a4d6a2ec819f796fbe4c1735e61cd6168d41
Svetlana Kuro <svetlana.kuro@gmail.com>
2021-07-09 16:57:52 +0300

    Android1Lesson1 completed

--- app/src/main/java/com/example/newgbapplication/MainActivity.java
+++ app/src/main/java/com/example/newgbapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.newgbapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button= findViewById(R.id.switchLayout);
+        button.setOnClickListener(v -> setContentView(R.layout.activity_main_1));
     }
 }
